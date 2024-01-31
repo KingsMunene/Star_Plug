13a35875c7307583b78bc1d1cd4eef41b53d109b
KingsMunene <munenekings2@gmail.com>
2024-01-31 18:54:08 +0300

    Planet class for the characters

--- app/src/main/java/com/example/starplug/data/Character.kt
+++ app/src/main/java/com/example/starplug/data/Character.kt
@@ -1,3 +1,16 @@
 package com.example.starplug.data
 
-data class Character()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Character(
+    val name: String,
+    @SerialName(value = "birth_year")
+    val birthYear: String,
+    val height: String,
+    @SerialName(value = "homeworld")
+    val homePlanet: String,
+    val films: List<String>,
+    val species: String?
+)
