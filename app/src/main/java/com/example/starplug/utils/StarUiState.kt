0e0a8ffb0f1708dfe2023d939b155cd60de73eac
KingsMunene <munenekings2@gmail.com>
2024-02-03 10:30:27 +0300

    Added UiState sealed class used to communicate the state of the app

--- app/src/main/java/com/example/starplug/utils/StarUiState.kt
+++ app/src/main/java/com/example/starplug/utils/StarUiState.kt
@@ -1,4 +1,17 @@
 package com.example.starplug.utils
 
-class StarUiState {
+import com.example.starplug.data.Results
+import com.example.starplug.data.StarCharacter
+
+sealed class StarUiState {
+    object Loading: StarUiState()
+
+    object Error: StarUiState()
+
+    data class Success(
+        val characters: List<StarCharacter>,
+//        val planetUrl: String,
+//        val speciesUrl: String?,
+//        val filmsUrl: List<String>
+    ): StarUiState()
 }
